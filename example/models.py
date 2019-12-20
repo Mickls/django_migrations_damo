d9e05a37b4336b7ae7be6acc2c3a7d7947fa9864
成 蒋 <914541185@qq.com>
2019-12-20 14:13:27 +0800

    1

--- example/models.py
+++ example/models.py
@@ -2,3 +2,17 @@ from django.db import models
 
 
 # Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=30, verbose_name="名称")
+    address = models.CharField("地址", max_length=50)
+    city = models.CharField("城市", max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    class Meta:
+        verbose_name = "出版商"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
